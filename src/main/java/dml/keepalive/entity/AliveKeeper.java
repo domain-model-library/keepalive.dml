67dd93ec2e7fc2a07af3293787a543df15517b8b
zhengchengdong <zhengchengdong@gmail.com>
2025-10-11 11:16:33 +0800

    添加注释指导使用方设计

--- src/main/java/dml/keepalive/entity/AliveKeeper.java
+++ src/main/java/dml/keepalive/entity/AliveKeeper.java
@@ -1,5 +1,7 @@
 package dml.keepalive.entity;
 
+// 注意，具体项目中，应该是有专门的AliveKeeper实现类的，而不是用原有的业务类来充当AliveKeeper。
+// 比如，用户会话保活，应该新建一个 UserSessionAliveKeeper ，而不是用 UserSession 来充当 AliveKeeper 。
 public interface AliveKeeper {
     void setId(Object id);
 
